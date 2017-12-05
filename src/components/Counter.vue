c67db0375d4928dfc588912e7a330f10954c8670
lutao198 <lutao198@163.com>
2017-12-06 03:20:09 +0800

    第一个版本

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="">
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+		<p><span>{{ num }}</span></p>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		props:['num'],
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App',
+				
+			}
+		},
+		methods: {
+			increment() {
+				this.$emit('incre')
+			},
+			decrement() {
+				this.$emit('decre')
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
